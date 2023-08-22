36a1f748824882f0ef5482a32e27e33b6f526800
iamsoojung <crystar2402@naver.com>
2023-08-22 20:33:23 +0900

    initial commit

---
+++ src/test/java/com/example/agebloomersbackend/AgebloomersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agebloomersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgebloomersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
