557171bcce660803f960ada7646681bcf8112d92
johyewon <johyewon4601@gmail.com>
2023-09-04 04:15:35 +0900

    fix: 요양보호사 리스트 조회 API 내 쿼리 필터 적용

--- src/main/java/com/example/agebloomersbackend/repository/CaregiverMatchRepository.java
+++ src/main/java/com/example/agebloomersbackend/repository/CaregiverMatchRepository.java
@@ -2,5 +2,9 @@ package com.example.agebloomersbackend.repository;
 
 import com.example.agebloomersbackend.domain.CaregiverMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 public interface CaregiverMatchRepository extends JpaRepository<CaregiverMatch, Long> {
+    @Query("SELECT bm.caregiver.id FROM CaregiverMatch bm")
+    List<Long> findAllCaregiverIds();
 }
--- src/main/java/com/example/agebloomersbackend/service/CaregiversService.java
+++ src/main/java/com/example/agebloomersbackend/service/CaregiversService.java
@@ -1,25 +1,39 @@
 package com.example.agebloomersbackend.service;
 
-import com.example.agebloomersbackend.domain.Babysitters;
 import com.example.agebloomersbackend.domain.Caregivers;
-import com.example.agebloomersbackend.repository.BabysittersRepository;
 import com.example.agebloomersbackend.repository.CaregiversRepository;
+import com.example.agebloomersbackend.repository.CaregiverMatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CaregiversService {
     private CaregiversRepository caregiversRepository;
+    private CaregiverMatchRepository caregiverMatchRepository;
 
     @Autowired
-    public CaregiversService(CaregiversRepository caregiversRepository) {
+    public CaregiversService(CaregiversRepository caregiversRepository, CaregiverMatchRepository caregiverMatchRepository) {
         this.caregiversRepository = caregiversRepository;
+        this.caregiverMatchRepository = caregiverMatchRepository;
     }
 
     public List<Caregivers> getAllCaregivers() {
-        return caregiversRepository.findAll();
+
+        // CaregiverMatch 테이블에서 caregivers_id 가져옴
+        List<Long> caregiversIdsInCaregiverMatch = getCaregiversIdsInCaregiverMatch();
+
+        // Caregivers 테이블에서 해당 caregivers_id와 id가 동일하지 않은 모든 레코드를 선택
+        return caregiversRepository.findAll().stream()
+                .filter(caregivers -> !caregiversIdsInCaregiverMatch.contains(caregivers.getId()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Long> getCaregiversIdsInCaregiverMatch() {
+        // caregiver_matches 테이블의 caregiver_id 컬럼 값 조회
+        return caregiverMatchRepository.findAllCaregiverIds();
     }
 
     public Caregivers createCaregiver(Caregivers caregiver) {
