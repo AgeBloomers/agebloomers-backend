d0e3c872288f05a31b5b9a74247bf55473643399
johyewon <johyewon4601@gmail.com>
2023-09-03 20:59:30 +0900

    fix: registerdetails 도메인 수정

--- src/main/java/com/example/agebloomersbackend/domain/RegisterDetails.java
+++ src/main/java/com/example/agebloomersbackend/domain/RegisterDetails.java
@@ -8,35 +8,37 @@ import java.util.Date;
 public class RegisterDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @Temporal(TemporalType.DATE)
-    @Column(name="register_date")
+    @Column(name = "register_date", nullable = false)
     private Date registerDate;
 
+    @Column(name = "comment", nullable = true, length = 255)
     private String comment;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name="start_time")
+    @Column(name = "start_time", nullable = false)
     private Date startTime;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name="end_time")
+    @Column(name = "end_time", nullable = false)
     private Date endTime;
 
     @ManyToOne
-    @JoinColumn(name = "parent_id")
+    @JoinColumn(name = "parent_id", nullable = true)
     private Parents parent;
 
     @ManyToOne
-    @JoinColumn(name = "babysitter_id")
+    @JoinColumn(name = "babysitter_id", nullable = true)
     private Babysitters babysitter;
 
     @ManyToOne
-    @JoinColumn(name = "elder_id")
+    @JoinColumn(name = "elder_id", nullable = true)
     private Elders elder;
 
     @ManyToOne
-    @JoinColumn(name = "caregiver_id")
+    @JoinColumn(name = "caregiver_id", nullable = true)
     private Caregivers caregiver;
 }
