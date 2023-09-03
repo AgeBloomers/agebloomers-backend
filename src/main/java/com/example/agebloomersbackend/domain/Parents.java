71732baf073d601d7cd813ca9b67951a32d503da
iamsoojung <crystar2402@naver.com>
2023-09-03 20:18:53 +0900

    fix: parents 도메인 수정

--- src/main/java/com/example/agebloomersbackend/domain/Parents.java
+++ src/main/java/com/example/agebloomersbackend/domain/Parents.java
@@ -14,12 +14,25 @@ public class Parents {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private Integer age;
+
+    @Column(nullable = false)
     private Character gender;
+
+    @Column(nullable = false, length = 50)
     private String address;
+
+    @Column(nullable = false, length = 20)
     private Integer contact;
+
+    @Column(nullable = false, length = 255)
     private String email;
+
+    @Column(nullable = false, length = 255)
     private String password;
 
 }
