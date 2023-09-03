88eef2a116c22ee759b3ce4a7e9132122bf84f1d
iamsoojung <crystar2402@naver.com>
2023-09-04 06:17:01 +0900

    feat: 특정 노인, 요양보호사, 부모 정보 조회 API 생성

--- src/main/java/com/example/agebloomersbackend/repository/RegisterDetailsRepository.java
+++ src/main/java/com/example/agebloomersbackend/repository/RegisterDetailsRepository.java
@@ -13,9 +13,12 @@ public interface RegisterDetailsRepository extends JpaRepository<RegisterDetails
     @Query("SELECT rd.registerDate, rd.comment, rd.startTime, rd.endTime, rd.babysitter.id FROM RegisterDetails rd WHERE rd.babysitter.id = :id")
     List<Object[]> findByBabysitterId(@Param("id") Long id);
 
-    List<Object[]> findByCaregiverId(Long id);
+    @Query("SELECT rd.registerDate, rd.comment, rd.startTime, rd.endTime, rd.caregiver.id FROM RegisterDetails rd WHERE rd.caregiver.id = :id")
+    List<Object[]> findByCaregiverId(@Param("id") Long id);
 
-    List<Object[]> findByElderId(Long id);
+    @Query("SELECT rd.registerDate, rd.comment, rd.startTime, rd.endTime, rd.elder.id FROM RegisterDetails rd WHERE rd.elder.id = :id")
+    List<Object[]> findByElderId(@Param("id") Long id);
 
-    List<Object[]> findByParentId(Long id);
+    @Query("SELECT rd.registerDate, rd.comment, rd.startTime, rd.endTime, rd.parent.id FROM RegisterDetails rd WHERE rd.parent.id = :id")
+    List<Object[]> findByParentId(@Param("id") Long id);
 }
