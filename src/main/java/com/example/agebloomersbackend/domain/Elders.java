44090d628bbec4d0093ce48cfef71d1a7a77af6b
johyewon <johyewon4601@gmail.com>
2023-09-03 20:29:32 +0900

    fix: elders 도메인 수정

--- src/main/java/com/example/agebloomersbackend/domain/Elders.java
+++ src/main/java/com/example/agebloomersbackend/domain/Elders.java
@@ -15,11 +15,24 @@ public class Elders {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private Integer age;
+
+    @Column(nullable = false)
     private Character gender;
+
+    @Column(nullable = false, length = 50)
     private String address;
+
+    @Column(nullable = false, length = 20)
     private Integer contact;
+
+    @Column(nullable = false, length = 255)
     private String email;
+
+    @Column(nullable = false, length = 255)
     private String password;
 }
