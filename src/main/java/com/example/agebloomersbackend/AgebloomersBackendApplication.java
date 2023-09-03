7e23ac259658e34bd42ded647799ca65450f5673
iamsoojung <crystar2402@naver.com>
2023-09-04 02:13:32 +0900

    chore: queryDSL 추가

--- src/main/java/com/example/agebloomersbackend/AgebloomersBackendApplication.java
+++ src/main/java/com/example/agebloomersbackend/AgebloomersBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.agebloomersbackend"})
+//@ComponentScan(basePackages = {"com.example.agebloomersbackend"})
 public class AgebloomersBackendApplication {
 
 	public static void main(String[] args) {
