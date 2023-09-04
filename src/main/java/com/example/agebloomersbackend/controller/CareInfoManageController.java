98dbb12a7c84f139d02056967b70830de9b946bf
johyewon <johyewon4601@gmail.com>
2023-09-05 00:10:33 +0900

    feat: 케어 정보 삭제 API 생성

--- src/main/java/com/example/agebloomersbackend/controller/CareInfoManageController.java
+++ src/main/java/com/example/agebloomersbackend/controller/CareInfoManageController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/careinfo")
@@ -47,4 +49,13 @@ public class CareInfoManageController {
         Elders createdElders = careInfoManageService.createElder(elders);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdElders);
     }
+
+    @DeleteMapping("/{type}/{id}")
+    public List<String> deleteCareInfoByIdAndType(
+            @PathVariable Long id,
+            @PathVariable String type
+    ) {
+        return careInfoManageService.deleteCareInfoByIdAndType(id, type);
+    }
+
 }
--- src/main/java/com/example/agebloomersbackend/service/CareInfoManageService.java
+++ src/main/java/com/example/agebloomersbackend/service/CareInfoManageService.java
@@ -6,16 +6,18 @@ import com.example.agebloomersbackend.domain.Elders;
 import com.example.agebloomersbackend.domain.Parents;
 import com.example.agebloomersbackend.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 @Service
 public class CareInfoManageService {
     private BabysittersRepository babysittersRepository;
     private ParentsRepository parentsRepository;
     private CaregiversRepository caregiversRepository;
     private EldersRepository eldersRepository;
+//    private CareInfoManageRepository careInfoManageRepository;
 
     @Autowired
     public CareInfoManageService(BabysittersRepository babysittersRepository, ParentsRepository parentsRepository,
@@ -26,6 +28,7 @@ public class CareInfoManageService {
         this.eldersRepository = eldersRepository;
     }
 
+    // 등록
     public Babysitters createBabysitter(Babysitters babysitters) {
         return babysittersRepository.save(babysitters);
     }
@@ -40,5 +43,25 @@ public class CareInfoManageService {
 
     public Parents createParent(Parents parent) {
         return parentsRepository.save(parent);
+    }
+
+    // 삭제
+    public List<String> deleteCareInfoByIdAndType(Long id, String type) {
+        switch (type) {
+            case "Babysitters":
+                Babysitters babysitters = babysittersRepository.findById(id).orElse(null);
+                babysittersRepository.delete(babysitters);
+            case "Caregivers":
+                Caregivers caregivers = caregiversRepository.findById(id).orElse(null);
+                caregiversRepository.delete(caregivers);
+            case "Elders":
+                Elders elders = eldersRepository.findById(id).orElse(null);
+                eldersRepository.delete(elders);
+            case "Parents":
+                Parents parents = parentsRepository.findById(id).orElse(null);
+                parentsRepository.delete(parents);
+            default:
+                return List.of();
+        }
     }
 }
