e44079e14fafe54e9fbde296a05700745f85d07d
johyewon <johyewon4601@gmail.com>
2023-09-05 03:17:57 +0900

    fix: Parents 도메인 수정 (생성자 추가)

--- src/main/java/com/example/agebloomersbackend/domain/Parents.java
+++ src/main/java/com/example/agebloomersbackend/domain/Parents.java
@@ -39,4 +39,12 @@ public class Parents {
     public void setId(Long id) {
         this.id = id;
     }
+    public void setName(String name) {this.name = name;}
+    public void setAge(Integer age) {this.age = age;}
+    public void setGender(Character gender) {this.gender = gender;}
+    public void setAddress(String address) {this.address = address;}
+    public void setContact(String contact) {this.contact = contact;}
+    public void setEmail(String email) {this.email = email;}
+    public void setPassword(String password) {this.password = password;}
+
 }
