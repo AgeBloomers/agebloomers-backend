a723ff5bcb502a095b59fd859bd11f2ee330bbe0
johyewon <johyewon4601@gmail.com>
2023-09-06 06:23:37 +0900

    feat: Caregiver-Elder Match 상태 수정 API 생성

--- src/main/java/com/example/agebloomersbackend/controller/MatchManageController.java
+++ src/main/java/com/example/agebloomersbackend/controller/MatchManageController.java
@@ -23,18 +23,17 @@ public class MatchManageController {
         this.matchManageService = matchManageService;
     }
 
-//    @PutMapping("/edit/matchingstatus")
-//    public ResponseEntity<String> updateBMatchStatus(
-//            @PathVariable Long applicant_id,
-//            @PathVariable Long registrant_id,
-//            @PathVariable String type,
-//            @PathVariable Boolean status
-//    ) {
-//        try {
-//            matchManageService.updateBMatchStatus(applicant_id, registrant_id, type, status);
-//            return new ResponseEntity<>("정보 수정 성공", HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>("정보 수정 오류", HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+    @PutMapping("/matchStatus/{type}/{registrant_id}/{status}")
+    public ResponseEntity<String> updateBMatchStatus(
+            @PathVariable Long registrant_id,
+            @PathVariable String type,
+            @PathVariable Boolean status
+    ) {
+        try {
+            matchManageService.updateCaregiverMatch(registrant_id, type, status);
+            return new ResponseEntity<>("매칭 상태 수정 성공", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("매칭 상태 수정 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
---
+++ src/main/java/com/example/agebloomersbackend/service/MatchManageService.java
@@ -0,0 +1,59 @@
+package com.example.agebloomersbackend.service;
+
+import com.example.agebloomersbackend.domain.*;
+import com.example.agebloomersbackend.repository.BabysitterMatchRepository;
+import com.example.agebloomersbackend.repository.CaregiverMatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatchManageService {
+    private BabysitterMatchRepository babysitterMatchRepository;
+    private CaregiverMatchRepository caregiverMatchRepository;
+
+    @Autowired
+    public MatchManageService(BabysitterMatchRepository babysitterMatchRepository,
+                              CaregiverMatchRepository caregiverMatchRepository) {
+        this.babysitterMatchRepository = babysitterMatchRepository;
+        this.caregiverMatchRepository = caregiverMatchRepository;
+    }
+
+    public List<Object> updateCaregiverMatch(Long registrantId, String type, Boolean status) {
+        switch (type) {
+            case "Caregivers":
+                List<CaregiverMatch> Cmatches = caregiverMatchRepository.findByTypeAndCaregiverId(type, registrantId);
+
+                if (!Cmatches.isEmpty()) {
+                    for (CaregiverMatch match : Cmatches) {
+                        // status : 0 -> 레코드 삭제, 1 -> Completed로 업데이트
+                        if (status) {
+                            match.setStatus("Completed");
+                            caregiverMatchRepository.save(match);
+                        } else{
+                            caregiverMatchRepository.delete(match);
+                        }
+                    }
+                }
+                break;
+            case "Elders":
+                List<CaregiverMatch> Ematches = caregiverMatchRepository.findByTypeAndElderId(type, registrantId);
+
+                if (!Ematches.isEmpty()) {
+                    for (CaregiverMatch match : Ematches) {
+                        if (status) {
+                            match.setStatus("Completed");
+                            caregiverMatchRepository.save(match);
+                        } else{
+                            caregiverMatchRepository.delete(match);
+                        }
+                    }
+                }
+                break;
+            default:
+                return List.of();
+        }
+        return List.of();
+    }
+}
