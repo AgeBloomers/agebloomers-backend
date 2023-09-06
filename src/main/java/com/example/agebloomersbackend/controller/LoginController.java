c60f2c61702777d898537d45f60d305ed13fede8
iamsoojung <crystar2402@naver.com>
2023-09-06 18:57:06 +0900

    fix: 로그인 API 컨트롤러 수정

--- src/main/java/com/example/agebloomersbackend/controller/LoginController.java
+++ src/main/java/com/example/agebloomersbackend/controller/LoginController.java
@@ -1,13 +1,14 @@
 package com.example.agebloomersbackend.controller;
 
 import com.example.agebloomersbackend.service.LoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api")
 public class LoginController {
     private final LoginService loginService;
 
@@ -16,7 +17,7 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @PostMapping
+    @GetMapping("/login/{name}")
     public ResponseEntity<Object> login(@PathVariable String name,
                                         @PathVariable String password) {
         Object user = loginService.login(name, password);
